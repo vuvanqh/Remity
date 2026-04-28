ad0de7848ef85a353c41558d06a674d0eb22c466
BanhBaoz <q.vuvan05@gmail.com>
2026-04-28 14:04:21 +0200

    feat: add stock controller unit tests

---
+++ stock-market/src/modules/stock/stock.controller.spec.ts
@@ -0,0 +1,56 @@
+import { Test } from '@nestjs/testing';
+import { StockController } from './stock.controller';
+import { StockService } from './stock.service';
+
+describe('StockController', () => {
+
+    let controller: StockController;
+
+    const mockService = {
+        getStocks: jest.fn(),
+        setStocks: jest.fn(),
+    };
+
+    beforeEach(async () => {
+        jest.clearAllMocks();
+
+        const module = await Test.createTestingModule({
+            controllers: [StockController],
+            providers: [{
+                provide: StockService,
+                useValue: mockService
+            }]
+        }).compile();
+
+        controller = module.get(StockController);
+    });
+
+    it('should create controller', () => {
+        expect(controller).toBeDefined();
+    });
+
+    it('gets all stocks', async () => {
+        mockService.getStocks.mockResolvedValue([
+            {name:'stock1', quantity:10}
+        ]);
+
+        const result = await controller.getStocks();
+
+        expect(result).toEqual([{
+            name:'stock1',
+            quantity: 10
+        }]);
+        expect(mockService.getStocks).toHaveBeenCalledTimes(1);
+    });
+
+    it('sets stocks', async () => {
+        const request = [{
+            name:'stock1',
+            quantity:10
+        }];
+        mockService.setStocks.mockResolvedValue(undefined);
+
+        await controller.setStocks(request);
+        expect(mockService.setStocks).toHaveBeenCalledWith(request);
+    });
+});

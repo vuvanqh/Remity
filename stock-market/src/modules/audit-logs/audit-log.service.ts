b263b9bbba968438b7e7fe456763d16bd15a7a68
BanhBaoz <q.vuvan05@gmail.com>
2026-05-02 01:10:57 +0200

    fix: remove unnecessary read transaction causing invalid rollback state

--- stock-market/src/modules/audit-logs/audit-log.service.ts
+++ stock-market/src/modules/audit-logs/audit-log.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { AuditLogRepository } from './audit-log.repository';
-import { AuditLog, NewAuditLog } from '../../database/types/audit-log.types';
+import { NewAuditLog } from '../../database/types/audit-log.types';
 import { DbExecutor } from '../../database/types/dbContext';
-import { db } from '../../database/kysely.provider';
+import { AuditLogResponseDto } from './dtos/audit-log-response.dto';
 
 @Injectable()
 export class AuditLogService {
@@ -10,16 +10,23 @@ export class AuditLogService {
         private readonly auditLogRepository: AuditLogRepository,
     ){}
 
-    public async getAuditLogs(): Promise<AuditLog[]> {
-        return db.transaction()
-        .setAccessMode('read only')
-        .setIsolationLevel('snapshot')
-        .execute(async trx => {
-            return await this.auditLogRepository.getAuditLogs(trx);
+    public async getAuditLogs(): Promise<AuditLogResponseDto[]> {
+        let logs = await this.auditLogRepository.getAuditLogs();
+        await logs.sort((a, b) => {
+            a.createdAt = new Date(a.createdAt);
+            b.createdAt = new Date(b.createdAt);
+            return a.createdAt.getTime() - b.createdAt.getTime();
+        });
+        return logs.map(log => {
+            return {
+                type: log.type,
+                wallet_id: log.wallet_id,
+                stock_name: log.stock_name,
+            }
         });
     }
 
-    public async createAuditLog(auditLog: NewAuditLog, exec: DbExecutor = db): Promise<void> {
+    public async createAuditLog(auditLog: NewAuditLog, exec: DbExecutor): Promise<void> {
         await this.auditLogRepository.createAuditLog(auditLog, exec);
     }
 }
